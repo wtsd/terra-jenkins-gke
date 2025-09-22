5481473256130f842a5f028592faa7b2993ed9c6
wtsd <warlockfx@gmail.com>
2025-09-22 11:05:27 -0400

    Setting up gke

--- infra/provider.tf
+++ infra/provider.tf
@@ -10,7 +10,7 @@ terraform {
 
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.29"
+      version = "~> 2.0.0"
 
     }
 
@@ -29,3 +29,25 @@ provider "google" {
 
 data "google_client_config" "current" {}
 
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/guides/getting-started.html
+provider "kubernetes" {
+  #host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+# https://registry.terraform.io/providers/hashicorp/helm/latest/docs
+provider "helm" {
+    kubernetes = {
+        #host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+        host                   = google_container_cluster.primary.endpoint
+        token                  = data.google_client_config.provider.access_token
+        cluster_ca_certificate = base64decode(
+        data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,)
+        exec = {
+            api_version = "client.authentication.k8s.io/v1beta1"
+            command     = "gke-gcloud-auth-plugin"
+        }
+    }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+# GCP:
+variable "project_id" { type = string }
+variable "region" { type = string }
+variable "zone" { type = string }
+
